87f2fc1c1476b436c6550659453484f250dc8f29
Marcel Antal <marcel.antal@cs.utcluj.ro>
2021-04-05 11:51:35 +0300

    Added source code

---
+++ src/main/java/dao/AbstractDAO.java
@@ -0,0 +1,115 @@
+package dao;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import connection.ConnectionFactory;
+
+/**
+ * @Author: Technical University of Cluj-Napoca, Romania Distributed Systems
+ *          Research Laboratory, http://dsrl.coned.utcluj.ro/
+ * @Since: Apr 03, 2017
+ * @Source http://www.java-blog.com/mapping-javaobjects-database-reflection-generics
+ */
+public class AbstractDAO<T> {
+	protected static final Logger LOGGER = Logger.getLogger(AbstractDAO.class.getName());
+
+	private final Class<T> type;
+
+	@SuppressWarnings("unchecked")
+	public AbstractDAO() {
+		this.type = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+
+	}
+
+	private String createSelectQuery(String field) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("SELECT ");
+		sb.append(" * ");
+		sb.append(" FROM ");
+		sb.append(type.getSimpleName());
+		sb.append(" WHERE " + field + " =?");
+		return sb.toString();
+	}
+
+	public List<T> findAll() {
+		// TODO:
+		return null;
+	}
+
+	public T findById(int id) {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		String query = createSelectQuery("id");
+		try {
+			connection = ConnectionFactory.getConnection();
+			statement = connection.prepareStatement(query);
+			statement.setInt(1, id);
+			resultSet = statement.executeQuery();
+
+			return createObjects(resultSet).get(0);
+		} catch (SQLException e) {
+			LOGGER.log(Level.WARNING, type.getName() + "DAO:findById " + e.getMessage());
+		} finally {
+			ConnectionFactory.close(resultSet);
+			ConnectionFactory.close(statement);
+			ConnectionFactory.close(connection);
+		}
+		return null;
+	}
+
+	private List<T> createObjects(ResultSet resultSet) {
+		List<T> list = new ArrayList<T>();
+
+		try {
+			while (resultSet.next()) {
+				T instance = type.newInstance();
+				for (Field field : type.getDeclaredFields()) {
+					Object value = resultSet.getObject(field.getName());
+					PropertyDescriptor propertyDescriptor = new PropertyDescriptor(field.getName(), type);
+					Method method = propertyDescriptor.getWriteMethod();
+					method.invoke(instance, value);
+				}
+				list.add(instance);
+			}
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IntrospectionException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public T insert(T t) {
+		// TODO:
+		return t;
+	}
+
+	public T update(T t) {
+		// TODO:
+		return t;
+	}
+}
